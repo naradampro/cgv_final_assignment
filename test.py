6d6ba6c7901002a0d4f91c69c140be163808679c
waokwarachchi <82043184+waokwarachchi@users.noreply.github.com>
2023-08-15 14:32:03 +0530

    modifications in test.py class

--- test.py
+++ test.py
@@ -3,13 +3,13 @@ from student import StudentCollection
 if __name__ == "__main__":
     collection = StudentCollection("info.xml")
     
-    student_by_index = collection.find_by_index_no("1")
+    student_by_index = collection.find_by_index_no("001")
     if student_by_index:
         print(f"Student found by index number: {student_by_index.name}")
     else:
         print("Student not found by index number")
 
-    student_by_name = collection.find_by_name("John Doe")
+    student_by_name = collection.find_by_name("John Snow")
     if student_by_name:
         print(f"Student found by name: {student_by_name.index_no}")
     else:
