6729ac666ae6b54a72d0822745dcac54ffd1bb87
Narada Abeykoon <naradampro@gmail.com>
2023-09-13 14:26:26 +0530

    add table line extraction method

--- contours.py
+++ contours.py
@@ -26,13 +26,44 @@ def crop_image_to_table_area(image):
     return image[y:y+h, x:x+w]
 
 
+def extract_table_lines(img):
+    img_gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, (1, 1))
+    img_gray = cv.morphologyEx(img_gray, cv.MORPH_CLOSE, kernel)
+
+    (_, img_bin) = cv.threshold(img_gray,
+                                128, 255, cv.THRESH_BINARY | cv.THRESH_OTSU)
+    img_bin = cv.bitwise_not(img_bin)
+
+    kernel_length_v = (np.array(img_gray).shape[1]) // 120
+    vertical_kernel = cv.getStructuringElement(
+        cv.MORPH_RECT, (1, kernel_length_v))
+    im_temp1 = cv.erode(img_bin, vertical_kernel, iterations=3)
+
+    vertical_lines_img = cv.dilate(im_temp1, vertical_kernel, iterations=3)
+
+    kernel_length_h = (np.array(img_gray).shape[1]) // 120
+    horizontal_kernel = cv.getStructuringElement(
+        cv.MORPH_RECT, (kernel_length_h, 1))
+    im_temp2 = cv.erode(img_bin, horizontal_kernel, iterations=3)
+    horizontal_lines_img = cv.dilate(im_temp2, horizontal_kernel, iterations=3)
+
+    kernel = cv.getStructuringElement(cv.MORPH_RECT, (3, 3))
+    table_segment = cv.addWeighted(
+        vertical_lines_img, 0.5, horizontal_lines_img, 0.5, 0.0)
+    table_segment = cv.erode(cv.bitwise_not(
+        table_segment), kernel, iterations=2)
+    _, table_segment = cv.threshold(table_segment, 0, 255, cv.THRESH_OTSU)
+
+    return table_segment
+
+
 def extract_contours(image):
     imgray = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
     ret, thresh = cv.threshold(imgray, 127, 255, 0)
     contours, hierarchy = cv.findContours(
         thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-
-    print(hierarchy)
 
     return sorted(contours, key=cv.contourArea, reverse=True)
 
@@ -79,17 +110,29 @@ def plot_2img_compare(original_image, result):
 
 
 def plot_img(image, title="Image Title"):
-    plt.imshow(image)
+    plt.imshow(image, cmap=plt.cm.gray)
     plt.title(title)
     plt.show()
 
 
-# image = cv.imread("sign_sheets/1.jpeg")
-# # biggest_contour = extract_biggest_contour(image)
+def extract_sign_table(image):
+    cropped_image = crop_image_to_table_area(image)
+    table_line_image = extract_table_lines(cropped_image)
 
-# # result = perspective_transform(image, biggest_contour)
+    plot_img(table_line_image)
 
-# contours = extract_contours(image)
-# cv.drawContours(image, contours, -1, (0, 255, 0), 3)
+    contours, hierarchy = cv.findContours(
+        table_line_image, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
-# plot_img(image)
+    biggest_contour = contours[1]
+
+    return perspective_transform(cropped_image, biggest_contour)
+
+
+image = cv.imread("sign_sheets/3.jpeg")
+
+extracted_table = extract_sign_table(image)
+
+plot_img(extracted_table)
+
+# cv.imwrite("extracted5.jpeg", extracted_table)
--- test_cont.py
+++ test_cont.py
@@ -3,8 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import contours as utils
 
-image = cv.imread("sign_sheets/5.jpeg")
-
+image = cv.imread("sign_sheets/1.jpeg")
 cropped_image = utils.crop_image_to_table_area(image)
 
 utils.plot_2img_compare(image, cropped_image)
