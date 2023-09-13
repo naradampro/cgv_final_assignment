838a5572ac548708d5cb55b1f08e210ff454982b
Jayoda Halloluwa <68233906+jshalloluwa@users.noreply.github.com>
2023-09-13 23:32:44 +0530

    visualize attendance counts of all students

--- data_visualization/data_visualization.py
+++ data_visualization/data_visualization.py
@@ -18,6 +18,17 @@ def get_student_attendance_by_id(student_id):
     return None
 
 
+def get_all_students_attendance():
+    attendance_data = []
+    for data in sample_data:
+        attendance_data.append({
+            "student_id": data["student_id"],
+            "present": data["present"],
+            "total": data["present"] + data["absent"]
+        })
+    return attendance_data
+
+
 def plot_student_attendance_pie(student_id):
     student_data = get_student_attendance_by_id(student_id)
 
@@ -32,5 +43,27 @@ def plot_student_attendance_pie(student_id):
         plt.show()
 
 
+def plot_all_students_attendance_bar():
+    all_students_data = get_all_students_attendance()
+
+    student_ids = [data["student_id"] for data in all_students_data]
+    present_counts = [data["present"] for data in all_students_data]
+    absent_counts = [data["total"] - data["present"] for data in all_students_data]
+
+    colors = ['#66b3ff', '#ff9999']
+
+    plt.figure(figsize=(10, 6))
+    plt.bar(student_ids, present_counts, label='Present', color=colors[0])
+    plt.bar(student_ids, absent_counts, bottom=present_counts, label='Absent', color=colors[1])
+    plt.title('Attendance Summary for All Students')
+    plt.xlabel('Student ID')
+    plt.ylabel('Attendance Count')
+    plt.legend()
+    plt.xticks(student_ids)
+    plt.tight_layout()
+    plt.show()
+
+
 student_id = 3
 plot_student_attendance_pie(student_id)
+plot_all_students_attendance_bar()
