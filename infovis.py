06ad19c323fdd2c718e102984731f70457b4a7df
Ayodhya12 <dilkiayodhya2@gmail.com>
2023-09-14 21:43:07 +0530

    Remove db files

--- infovis.py
+++ infovis.py
@@ -1,5 +1,7 @@
 import sys
 import db.dbmodel as db
+import display_utils.data_visualization as visualize
+import matplotlib.pyplot as plt
 
 
 def arguments():
@@ -12,6 +14,25 @@ def arguments():
 
 if __name__ == "__main__":
     student_id = arguments()
-    print(student_id)
-    attendance = db.get_attendance_records_by_student_id(student_id)
-    print(attendance)
+    attendance_records = db.get_attendance_records_by_student_id(student_id)
+    is_present_counts = {'Present': 0, 'Absent': 0}
+
+    for record in attendance_records:
+        if record.is_present:
+            is_present_counts['Present'] += 1
+        else:
+            is_present_counts['Absent'] += 1
+
+    # Create a pie chart
+    labels = list(is_present_counts.keys())
+    sizes = list(is_present_counts.values())
+    colors = ['#74ed7c', '#ed7474']
+    explode = (0.1, 0)  # explode the 'Present' slice
+
+    plt.pie(sizes, labels=labels, colors=colors,
+            autopct='%1.1f%%', startangle=140)
+    # Equal aspect ratio ensures that pie is drawn as a circle.
+    plt.axis('equal')
+
+    plt.title(f"Attendance Distribution for Student ID {student_id}")
+    plt.show()
